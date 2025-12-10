1e73a79b4e4c63c264bf47746ff81b29ed8a6801
Vinnzz-coy <kevinnazar76@gmail.com>
2025-12-10 20:30:55 +0700

    update login dan database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\LoginController; // Tambahkan ini
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect('/dashboard');
+    }
     return view('auth.login');
+})->name('login');
+
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
